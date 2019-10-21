659ccf9366353b9173108496d2d8fe77ce1c21a0
Amit Magen <amitmagen50@gmail.com>
2019-10-21 13:07:21 +0300

    first save

---
+++ 191011/file.java
@@ -0,0 +1,28 @@
+import jeliot.io.*;
+
+public class MyClass {
+    public static void main() {
+            int a;
+            int b;
+            int c;
+            int d;
+            
+            a=3;
+            b=7;
+            c=9;
+            d=18;
+            
+            if(a<b)
+            max=a;
+            else
+            max=b;
+            
+            if (d<max)
+            max=c;
+            
+            if (d<max)
+            max=d;
+            System.out.println ( "the smallest number is" + max);
+
+    }
+}
