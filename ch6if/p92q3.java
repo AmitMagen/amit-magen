06bcb621cc40751c97049de86cbb1593a5688c74
Amit Magen <amitmagen50@gmail.com>
2019-10-28 21:28:27 +0200

    homework

---
+++ ch6if/p92q3.java
@@ -0,0 +1,53 @@
+if 1-
+scanner reader= new scanner (system.in);
+int avg;
+system.out.println("enter a grade");
+avg=reader.nextint();
+if (avg==100)
+System.out.println ("כל הכבוד");
+if 2-
+int testmark;
+if (testmark < 60)
+testmark= testmark+ (testmark/10);
+System.out.println ("your test mark after a bonus is" + testmark);
+if 3-
+int a;
+int b;
+if (a>b)
+System.out.println ("smallest number is" + b);
+else
+System.out.println ("smallest number is" + a);
+if 4-
+scanner reader= new scanner (system.in);
+int num;
+system.out.println("enter a number diffrent from zero");
+num=reader.nextint();
+if(num>0)
+system.out.println("num is positive");
+else
+system.out.println("num is negative")
+if 5-
+scanner reader= new scanner(system.in);
+int a,b;
+system.out.println("enter a num");
+a=reader.nextInt();
+system.out.println("enter another num");
+b=reader.nextInt();
+if (a==b)
+system.out.println("the numbers are equal");
+else
+a=a+1;
+b=b+1;
+system.out.println(a+" "+b);
+if 6 -
+int zavit1,zavit2,zavit3;
+system.out.println("enter zavit");
+zavit1=readr.nextInt();
+system.out.println("enter zavit");
+zavit2=readr.nextInt();
+system.out.println("enter zavit");
+zavit3=readr.nextInt();
+if (zavit1+zavit2+zavit3==180)
+system.out.println ("triangle is possible");
+else
+system.out.println("triangle is impossible");
